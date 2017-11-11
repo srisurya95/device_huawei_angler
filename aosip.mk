ea1974678245f9f93519d47aa29ad7511fbe1d4a
srisurya95 <srisurya95@gmail.com>
2017-11-11 10:14:04 -0500

    Angler: Update fingerprint to OPR5.170623.007/4302479

--- aosip.mk
+++ aosip.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := Huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:8.0.0/OPR6.170623.013/4283548:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 8.0.0 OPR6.170623.013 4283548 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:8.0.0/OPR5.170623.007/4302479:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 8.0.0 OPR5.170623.007 4302479 4299446 release-keys"
